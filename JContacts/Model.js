b395e12ce51f476d50cd5d5874f541fe6ba443eb
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2013-03-29 09:54:08 +0900

    Update #2

---
+++ JContacts/Model.js
@@ -0,0 +1,25 @@
+﻿
+guidedModel =// @startlock
+{
+	会社 :
+	{
+		methods :
+		{// @endlock
+			testc1:function()
+			{// @lock
+				alert("Hello");
+			}// @startlock
+		},
+		collectionMethods :
+		{// @endlock
+			test1:function()
+			{// @lock
+				alert("Hello");
+			},// @lock
+			試験１:function()
+			{// @lock
+				Document.write("Hello");
+			}// @startlock
+		}
+	}
+};// @endlock
